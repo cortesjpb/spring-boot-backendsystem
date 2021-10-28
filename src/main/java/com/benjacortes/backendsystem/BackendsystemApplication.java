6e07f4604867464aa010dd9f57e2a86f4593bab1
Benjamin Cortes <cortesjpb@gmail.com>
2021-10-28 14:55:12 -0300

    configuration and example for creating a topic when application starts

--- src/main/java/com/benjacortes/backendsystem/BackendsystemApplication.java
+++ src/main/java/com/benjacortes/backendsystem/BackendsystemApplication.java
@@ -1,7 +1,10 @@
 package com.benjacortes.backendsystem;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class BackendsystemApplication {
@@ -10,4 +13,8 @@ public class BackendsystemApplication {
 		SpringApplication.run(BackendsystemApplication.class, args);
 	}
 
+	@Bean
+	NewTopic hobbit2() {
+		return TopicBuilder.name("un-nuevo-topico").partitions(3).replicas(2).build();
+	}
 }
