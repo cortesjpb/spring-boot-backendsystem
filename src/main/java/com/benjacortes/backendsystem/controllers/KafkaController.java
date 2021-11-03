67b764d3695e092cf4998be954f52c811bf837b2
Benjamin Cortes <cortesjpb@gmail.com>
2021-11-03 19:11:34 -0300

    Consumer endpoint added

--- src/main/java/com/benjacortes/backendsystem/controllers/KafkaController.java
+++ src/main/java/com/benjacortes/backendsystem/controllers/KafkaController.java
@@ -1,5 +1,10 @@
 package com.benjacortes.backendsystem.controllers;
 
+import java.util.List;
+
+import com.benjacortes.backendsystem.service.ConsumerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,15 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaController {
 
+    @Autowired
     private KafkaTemplate<String, String> template;
 
-    public KafkaController(KafkaTemplate<String, String> template) {
-        this.template = template;
-    }
+    @Autowired
+    private ConsumerService myTopicConsumer;
+
 
     @GetMapping("/kafka/produce")
     public void produce(@RequestParam String message) {
         template.send("MyTopic", message);
     }
-    
+
+    @GetMapping("/kafka/messages")
+    public List<String> getMessages() {
+        return myTopicConsumer.getMessages();
+    }
+
 }
+
+    
+    
+
