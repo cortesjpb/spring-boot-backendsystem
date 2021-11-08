05b90a6b421c956bd1906eeb20959c1418038413
Benjamin Cortes <cortesjpb@gmail.com>
2021-11-07 21:18:54 -0300

    flink topic listener added

--- src/main/java/com/benjacortes/backendsystem/BackendsystemApplication.java
+++ src/main/java/com/benjacortes/backendsystem/BackendsystemApplication.java
@@ -1,23 +1,20 @@
 package com.benjacortes.backendsystem;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
+
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 
 @SpringBootApplication
 public class BackendsystemApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendsystemApplication.class, args);
@@ -31,6 +28,10 @@ public class BackendsystemApplication {
 	NewTopic autorizePaymentTopic() {
 		return TopicBuilder.name("authorizePayment").partitions(3).replicas(2).build();
 	}
+	@Bean
+	NewTopic flinkOutput() {
+		return TopicBuilder.name("flink-output").partitions(3).replicas(2).build();
+	}
 
 	@Autowired
     private KafkaTemplate<String, String> template;
@@ -40,10 +41,15 @@ public class BackendsystemApplication {
 	public void createPaymentListener(String message) {
 		System.out.format("TOPICO createPayment: Me llegó el siguiente payment \n %s\n\n", message);
 		template.send("authorizePayment", message);
-
 	}
 	@KafkaListener(topics = "authorizePayment", groupId = "kafka-sandbox")
 	public void authorizePaymentListener(String message) {
 		System.out.format("TOPICO authorizePayment: Me llegó el siguiente payment \n %s\n\n", message);
+	}
+
+
+	@KafkaListener(topics = "flink-output", groupId = "kafka-sandbox")
+	public void flinkOutputListener(String message) {
+		System.out.format("TOPICO flink-output: Me llegó el siguiente payment \n %s\n\n", message);
 	}
 }
